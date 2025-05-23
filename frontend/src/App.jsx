ae563bf7866fbf2051352fc929f35616063cbc64
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-05-23 19:13:36 +0530

    feat: implement App component to structure main application layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Newsletter from './components/Newsletter'
+import ProductShowcase from './components/ProductShowcase'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <MainContent/>
+      <ProductShowcase/>
+      <Newsletter/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
