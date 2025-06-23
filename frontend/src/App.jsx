e4336931800679ba7ce761d030f23910fad2bd9d
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-06-23 11:49:29 +0530

    feat: restructure App component to include routing and main pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,36 @@
-import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Newsletter from './components/Newsletter'
-import ProductShowcase from './components/ProductShowcase'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Cart from './components/Cart';
 
 function App() {
-
   return (
-    <>
-      <Header/>
-      <MainContent/>
-      <ProductShowcase/>
-      <Newsletter/>
-      <Footer/>
-    </>
-  )
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
